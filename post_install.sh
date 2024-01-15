f921299d67a39594e793ed955ad5ffa3f62fb9db
Ali Darijani <ali.darijani.me@gmail.com>
2024-01-15 05:50:22 +0100

    usual update

--- post_install.sh
+++ post_install.sh
@@ -2,8 +2,10 @@
 
 # pacman stuff
 sudo pacman-mirrors --geoip && sudo pacman -Syyu --noconfirm
+sudo pacman -S base-devel --needed --noconfirm
+
 cli_text_editors="vi vim"
-sudo pacman -S  $cli_text_editors --needed --noconfirm
+sudo pacman -S $cli_text_editors --needed --noconfirm
 terminal_emulators="wezterm kitty alacritty"
 sudo pacman -S $terminal_emulators --needed --noconfirm
 sudo pacman -S zsh --needed --noconfirm
@@ -33,24 +35,18 @@ sudo pamac install zoom --no-confirm
 sudo pamac install teams --no-confirm
 sudo pamac install autojump --no-confirm
 
-
-
-
-
 # installing visual studio code extensions
 code --install-extension yzhang.markdown-all-in-one
+if [ -d ~/.oh-my-zsh ]; then
+    echo "The oh-my-zsh is probably already installed..."
+else
+    echo "The oh-my-zsh is probably not installed...installing it now..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-
-
-
-
-
-
-
-
-
-
-
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    echo "The zsh-syntax-highlighting is probably already installed..."
+else
+    echo "The zsh-syntax-highlighting is probably not installed...installing it now"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
